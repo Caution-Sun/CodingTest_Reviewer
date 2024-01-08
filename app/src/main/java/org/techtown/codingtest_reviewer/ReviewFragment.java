77a80fe063e1a14df3b98730d5af029d9cd16e44
Caution-Sun <hisbrjseka414>
2024-01-09 02:38:57 +0900

    UI : Make simple prototype UI

---
+++ app/src/main/java/org/techtown/codingtest_reviewer/ReviewFragment.java
@@ -0,0 +1,20 @@
+package org.techtown.codingtest_reviewer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ReviewFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_review, container, false);
+
+        return rootView;
+    }
+}
---
+++ app/src/main/java/org/techtown/codingtest_reviewer/SettingsFragment.java
@@ -0,0 +1,13 @@
+package org.techtown.codingtest_reviewer;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+    }
+}
