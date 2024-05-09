5bc28b1e9a25dfbfb67321634952af9b51635c54
Caution-Sun <hisbrjseka414>
2024-05-09 16:38:21 +0900

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# CodingTest_Reviewer
-코딩 테스트 복습용 앱
--- app/src/main/java/org/techtown/codingtest_reviewer/OnTestClickListener.java
+++
@@ -1,7 +0,0 @@
-package org.techtown.codingtest_reviewer;
-
-import android.view.View;
-
-public interface OnTestClickListener {
-    public void onItemClick(TestAdapter.ViewHolder holder, View view, int position);
-}
